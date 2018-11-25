0d6ddf189ad587a76c1f38fb55e22bb2c280235c
Wlodek Bzyl <matwb@univ.gda.pl>
2018-11-25 16:34:17 +0100

    Update texttest_fixture.rb

--- ruby/texttest_fixture.rb
+++ ruby/texttest_fixture.rb
@@ -14,9 +14,11 @@ items = [
   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
   # This Conjured item does not work properly yet
   Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
+  Item.new(name="Conjured Mana Cake", sell_in=3, quality=5)
+  Item.new(name="Conjured Mana Cake", sell_in=-1, quality=6)
 ]
 
-days = 2
+days = 17
 if ARGV.size > 0
   days = ARGV[0].to_i + 1
 end
